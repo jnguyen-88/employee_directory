98ef3037f16de94e293d34edb2e07fe63e8e4c3c
Justin Nguyen <justinhnguyen88@gmail.com>
2019-02-08 13:56:23 +0800

    add overlay func

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ Variables
 ================*/
 const url = 'https://randomuser.me/api/?results=12&nat=au';
 const $employeesContainer = $('#employees');
-const overlay = $('#overlay');
+const $overlay = $('#overlay');
 
 /*==============
 Fetch Function
@@ -12,11 +12,14 @@ Fetch Function
 fetch(url)
   .then(response => response.json())
   .then(data => parseData(data))
-
-
-
-
-
+  .then(function() {
+    const cards = document.querySelectorAll('.card');
+    for(let i = 0; i < cards.length; i++) {
+      cards[i].addEventListener("click", function(){
+      alert("hello")
+  });
+}
+  })
 
 
 /*==============
@@ -25,36 +28,35 @@ Helper Functions
 
 function parseData(data) {
   console.log(data);
-  let html;
+  let html = [];
   const employees = data.results;
 
-
-        /* <div class="card">
-            <img src="member-1.jpg" alt="member-1">
-            <div class="user-info">
-              <div class="name">Judy Nguyen</div>
-              <div class="email">jnguyen@example.com</div>
-              <div class="city">Sacramento</div>
-            </div>
-          </div> */
-
   //Generate HTML to append to DIV
-  employees.forEach((employee) => {
-    html += '<div class="card">';
-    html += `<img src=${employee.picture.medium} alt=${employee.name.first}>`; 
-    html += '<div class="user-info">';
-    html += `<div class="name">${capitalize(employee.name.first)}` + " " + `${capitalize(employee.name.last)}</div>`;
-    html += `<div class="email">${employee.email}</div>`;
-    html += `<div class="city">${capitalize(employee.location.city)}</div>`;
-    html += '</div>';
-    html += '</div>';
-  })
+  for(let i = 0; i < employees.length; i++){
+    html += `
+      <div class="card">
+        <img src=${employees[i].picture.medium} alt=${employees[i].name.first}>
+        <div class="user-info">
+          <div class="name">${capitalize(employees[i].name.first)} `  +
+          `${capitalize(employees[i].name.last)}</div>
+          <div class="email">${employees[i].email}</div>
+          <div class="city">${capitalize(employees[i].location.city)}</div>
+        </div>
+      </div>
+    `
+  }
   $employeesContainer.html(html);
 };
+
+
 
 function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
+
+
+
+
 
 
 
