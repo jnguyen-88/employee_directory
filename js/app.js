27532ee66d360ed989a06fde2f3147121a5b9d47
Justin Nguyen <justinhnguyen88@gmail.com>
2019-02-09 12:44:49 +0800

    refactor code

--- js/app.js
+++ js/app.js
@@ -13,35 +13,9 @@ Fetch Functions
 ================*/
 
 fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    employees = data.results;
-    for(let i = 0; i < employees.length; i++){
-      const card = document.createElement('div');
-      card.id = i;
-      card.className = 'card';
-      card.innerHTML = 
-      `
-          <img src=${employees[i].picture.medium} alt=${employees[i].name.first}>
-          <div class="user-info">
-            <div class="name">${capitalize(employees[i].name.first)} `  +
-            `${capitalize(employees[i].name.last)}</div>
-            <div class="email">${employees[i].email}</div>
-            <div class="city">${capitalize(employees[i].location.city)}</div>
-          </div>
-      `
-      employeesContainer.appendChild(card);
-    }
-  })
-  .then(function(){
-    const cards = document.querySelectorAll('.card');
-    for(let i = 0; i < cards.length; i++){
-      cards[i].addEventListener("click", function(){
-        cardIndex = this.id;
-        generateModalHtml(employees, cardIndex)
-      })
-    }
-  })
+  .then(parseJSON)
+  .then(createCards)
+  .then(clickCard)
 
 
 
@@ -49,55 +23,76 @@ fetch(url)
  Helper Functions
 ================*/
 
-function parseData(data) {
-  let html = [];
-  const employees = data.results;
-  //Generate HTML to append to DIV
-  for(let i = 0; i < employees.length; i++){
-    const card = document.createElement('div');
-    card.id = i;
-    card.className = 'card';
-    card.innerHTML = 
-    `
-        <img src=${employees[i].picture.medium} alt=${employees[i].name.first}>
-        <div class="user-info">
-          <div class="name">${capitalize(employees[i].name.first) + " " + capitalize(employees[i].name.last)}</div>
-          <div class="email">${employees[i].email}</div>
-          <div class="city">${capitalize(employees[i].location.city)}</div>
-        </div>
-    `
-      employeesContainer.appendChild(card);
-  }
+function parseJSON(response){
+  return response.json();
 };
 
-function generateModalHtml(employee, i) {
-  console.log(employee)
+function createCards(data) {
+    employees = data.results;
+    for(let i = 0; i < employees.length; i++){
+      const card = document.createElement('div');
+      let employee = employees[i];
+      card.id = i;
+      card.className = 'card';
+      let firstName = capitalize(employee.name.first);
+      let lastName = capitalize(employee.name.last);
+      let email = employee.email;
+      let city = capitalize(employee.location.city)
+      card.innerHTML = 
+      `
+          <img src=${employee.picture.medium} alt=${firstName}>
+          <div class="user-info">
+            <div class="name">${firstName + " " + lastName}</div>
+            <div class="email">${email}</div>
+            <div class="city">${city}</div>
+          </div>
+      `
+      employeesContainer.appendChild(card);
+    }
+}
+
+function clickCard() {
+  const cards = document.querySelectorAll('.card');
+  for(let i = 0; i < cards.length; i++){
+    cards[i].addEventListener("click", function(){
+      cardIndex = this.id;
+      generateModalHtml(employees, cardIndex)
+    })
+  }
+}
+
+function generateModalHtml(employees, i) {
   const modal = document.createElement('div');
-  let date = formatDate(employee[i].dob.date);
+  let date = formatDate(employees[i].dob.date);
+  let firstName = capitalize(employees[i].name.first);
+  let lastName = capitalize(employees[i].name.last);
+  let email = employees[i].email;
+  let city = capitalize(employees[i].location.city);
+  let state = capitalize(employees[i].location.state);
+  let postCode = employees[i].location.postcode;
+  let cell = employees[i].cell;
+  let street = capitalize(employees[i].location.street);
   modal.id = i;
   modal.className = 'modal-card';
   modal.innerHTML =
       `
         <span class="close">X</span>
-        <img src="${employee[i].picture.medium}" alt=>
+        <img src="${employees[i].picture.medium}" alt=>
           <div class="user-info">
-          <div class="name">${capitalize(employee[i].name.first) + " " + capitalize(employee[i].name.last)}</div>
-          <div class="email">${employee[i].email}</div>
-          <div class="city">${capitalize(employee[i].location.city) + ", " + capitalize((employee[i].location.state)) + " " + (employee[i].location.postcode)}</div>
+          <div class="name">${firstName + " " + lastName}</div>
+          <div class="email">${email}</div>
+          <div class="city">${city + ", " + state + " " + postCode}</div>
         </div>
         <div class="details">
-          <div class="info">${employee[i].cell}</div>
-          <div class="info">${capitalize(employee[i].location.street) + " " + capitalize(employee[i].location.city)}
+          <div class="info">${cell}</div>
+          <div class="info">${street + " " + city}
           </div>
           <div class="info">Birthday: ${date}</div>
         </div>
         `
         $overlay.show();
         $overlay.html(modal);
-        $('.close').click(function() {
-    $('#overlay').css('display', 'none');
-    $('.modal-card').remove();
-  })
+        $('.close').click(closeModal)
 }
 
 
@@ -111,12 +106,9 @@ function capitalize(str) {
     });
 }
 
-function addEventListenersToModal() {
-  // close button listener
-  $('.close').click(function() {
-    $('#overlay').css('display', 'none');
-    $('.modal-card').remove();
-  })
+function closeModal() {
+  $('#overlay').css('display', 'none');
+  $('.modal-card').remove();
 }
 
 function formatDate(day) {
